a3756a8f7d69b3f69e4e4dbc3427a223ec9881e7
Sandeep Madhavrao Patil <85793480+sandeep-m-patil@users.noreply.github.com>
2024-01-21 13:48:22 +0530

    Update MATRIX(x).c

--- ALL C PROGRAMSS/CONTROL_STATEMENTS/MATRIX(x).c
+++ ALL C PROGRAMSS/CONTROL_STATEMENTS/MATRIX(x).c
@@ -1,100 +1,39 @@
 #include<stdio.h>
+int main(){
+    int r1,c1,r2,c2,i,j,k;
+    printf("Enter no.of rows and cols for matrix A : ");
+    scanf("%d %d",&r1,&c1);
+    int a[r1][c1];
+    printf("Enter elements for matrix A: ");
+    for(i=0;i<r1;i++){
+        for(j=0;j<c1;j++)scanf("%d",&a[i][j]);
+    }
+    printf("Enter no.of rows and cols for matrix B : ");
+    scanf("%d %d",&r2,&c2);
+    int b[r2][c2];
+    printf("Enter elements for matrix B: ");
+    for(i=0;i<r2;i++){
+        for(j=0;j<c2;j++)scanf("%d",&b[i][j]);
+    }
 
-int main()
-{
-    int p,q,r,s,i,j;
-    printf("\tmat A x mat B calculator \n\n");
-    printf("Enter no.of rows for mat A : ");
-    scanf("%d",&p);
-    printf("Enter no.of cols for mat A : ");
-    scanf("%d",&q);
-    printf("Enter no.of rows for mat B : ");
-    scanf("%d",&r);
-    printf("Enter no.of cols for mat B : ");
-    scanf("%d",&s); 
- if(q==r){   
-    int a[p][q],b[r][s];
-    
-    
-    printf("\nEnter elements for mat A : ");
-    for(i=0;i<p;i++){
-     for(j=0;j<q;j++){
-     
-     scanf("%d",&a[i][j]);
-    
-     }
-    
+    int c[c1][r2];
+    for(i=0;i<r1;i++){
+        for(j=0;j<c2;j++)c[i][j]=0;
     }
-    printf("\nEnter elements for mat B : ");
-    for(i=0;i<r;i++){
-     for(j=0;j<s;j++){
-     
-     scanf("%d",&b[i][j]);
-    
-     }
-    
+
+    for(i=0;i<r1;i++){
+        for(j=0;j<c2;j++){
+            for(k=0;k<r2;k++){
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
     }
-    
-    printf("\nElements for mat A : \n");
-    for(i=0;i<p;i++){
-     for(j=0;j<q;j++){
-     
-     printf("%d   ",a[i][j]);
-    
-     }
-     printf("\n");
-    
-    }
-    
-    printf("\nElements for mat B : \n");
-    for(i=0;i<r;i++){
-     for(j=0;j<s;j++){
-     
-     printf("%d   ",b[i][j]);
-    
-     }
-     printf("\n");
-    
-    }
-    int c[p][r];
-    for(i=0;i<p;i++){
-     for(j=0;j<r;j++){
-     
-     c[i][j]=0;
-    
-     }
-     
-    
-    }
-    
-    
-    
-    for(i=0;i<q;i++){
-     for(j=0;j<s;j++){
-     for(int k=0;k<r;k++){
-     c[i][j]+=a[i][k]*b[k][j];
-     
-     }
-     
-    
-     }
-     
-    
-    }
-    
-    printf("\nElements of mat A x mat B : \n");
-    for(i=0;i<p;i++){
-     for(j=0;j<s;j++){
-     
-     printf("%d   ",c[i][j]);
-    
-     }
-     printf("\n");
-    
-    }
-    }
-    
-    else{
-    printf("\n mat A x mat B  not possible \n");
+
+    printf("The Multiplication of matrices A and B ( A x B ) is \n");
+    for(i=0;i<r1;i++){
+        for(j=0;j<c2;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
     }
 }
